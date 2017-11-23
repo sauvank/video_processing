85f01a28ee0ac6c10b1dc0dcbc10be05663842fe
Your Name <you@example.com>
2017-11-23 11:45:36 +0100

    push

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 include "./vendor/autoload.php";
 
 
-$pathToScan = '/media/sauvank/Film/movies/Action/';
+//$pathToScan = '/media/sauvank/Film/movies/Action/';
+$pathToScan = './video/';
 $pathImg = './img/';
-$pathVideo = './video/';
+$pathVideo = './videos_mp4/';
 //splitVideo('test.mkv','00:00:05','00:00:10','output-movie.mkv');
 
 $allFiles = scanFolder($pathToScan,'avi|mp4|mkv');
@@ -12,35 +13,27 @@ $allFiles = scanFolder($pathToScan,'avi|mp4|mkv');
 foreach($allFiles as $key => $value){
 
     $fullPathVideo = $value['dirname'].'/'.$value['basename'];
-    $timeToShoot = '400';
+    $timeToShoot = '10';
     $pathOutputImg = $pathImg.$value['filename'].'.png';
-//    getImageFromVideo($fullPathVideo,$timeToShoot,$pathOutputImg);
+    var_dump($fullPathVideo);
+    getImageFromVideo($fullPathVideo,$timeToShoot,$pathOutputImg);
 
 
-    $timeBegin = '00:30:00';
-    $timeStop = '00:00:10';
-    $pathOutput = $pathVideo.$value['filename'].'.mp4';
-    splitVideo($fullPathVideo, $timeBegin, $timeStop, $pathOutput);
+//    $timeBegin = '00:00:00';
+//    $timeStop = '00:00:10';
+//    $pathOutput = $pathVideo.$value['filename'].'.mp4';
+//    splitVideo($fullPathVideo, $timeBegin, $timeStop, $pathOutput);
 
 }
-
-
-
-
-
 
 function splitVideo($fullPathVideo, $timeBegin, $timeStop, $pathOutput){
-    var_dump($fullPathVideo);
-//    exec('ffmpeg -i '.$titleVideo.' -c copy -ss '.$timeBegin.' -t '.$timeStop.' '.$nameOutput.' -nostats -loglevel 0  --assume-yes');
-//    exec('ffmpeg -i '.$fullPathVideo.' -ss '.$timeBegin.' -t '.$timeStop.' -f mp4 -vcodec libx264 -preset fast -profile:v main -acodec aac '.$nameOutput.' -hide_banner');
-    exec("ffmpeg -i  '$fullPathVideo'  -ss 00:15:00 -t 00:00:10 -vcodec copy -acodec copy '$pathOutput'");
+    $cmd = "ffmpeg -i  '$fullPathVideo' -f mp4 -vcodec libx264 -preset fast -profile:v main -acodec aac -strict -2 -ss $timeBegin -t $timeStop '$pathOutput'  -nostats -loglevel 0 -hide_banner";
+    exec($cmd);
 }
 
-function getImageFromVideo($pathVideo,$timeToShoot, $outPicure){
-    $ffmpeg = FFMpeg\FFMpeg::create();
-    $video = $ffmpeg->open($pathVideo);
-    $frame = $video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($timeToShoot));
-    $frame->save($outPicure);
+function getImageFromVideo($pathVideo,$timeToShoot, $outPicture){
+   $cmd = "ffmpeg -i '$pathVideo' -ss '$timeToShoot'  -vframes 1 -q:v 2 '$outPicture'";
+   exec($cmd);
 }
 
 function scanFolder($path,$extention){
